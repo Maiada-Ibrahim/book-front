0a6c5a38776941d57c3f39c37254b377f81a6fa6
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-22 17:15:05 +0300

    finish with error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
+// import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
+import Login from './Login';
+import { withAuth0 } from '@auth0/auth0-react';
+import BestBooks from './BestBooks';
+import Profile from './compo/Profile';
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,20 +19,26 @@ class App extends React.Component {
     return(
       <>
         <Router>
-          <IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
             <Header />
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {
+                  this.props.auth0.isAuthenticated ? <BestBooks />: <Login/>
+                }
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route  path="/profile"> <Profile/> </Route>
             </Switch>
             <Footer />
-          </IsLoadingAndError>
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
   }
 }
 
-export default App;
+// export default App;
+export default withAuth0(App);
+
--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
+import LoginButton from './compo/LoginButton';
+import LogoutButton from './compo/LogoutButton';
+import { withAuth0 } from '@auth0/auth0-react';
+// import withAuth0(Profile) from './compo/Profile';
+
+
+
+
 import './Header.css';
 
 class Header extends React.Component {
@@ -12,9 +20,14 @@ class Header extends React.Component {
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+         {/* ({this.props.auth0.isAuthenticated} == true)? <LogoutButton/> :  <LoginButton/> */}
+               {
+                  this.props.auth0.isAuthenticated ? <LogoutButton/>:<LoginButton/>
+              }
       </Navbar>
     );
   }
 }
 
-export default Header;
+// export default Header;
+export default withAuth0(Header);
